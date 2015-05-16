962096edab521e4e1d403d80aedcb506b5c004a7
michaelfortuna <m@ninjastack.com>
2015-05-16 02:27:59 -0400

    added avatar psd

--- src/js/controllers/LandingCtrl.js
+++ src/js/controllers/LandingCtrl.js
@@ -4,6 +4,18 @@ var module = require('./module');
 
 module.controller('LandingCtrl', function($scope, $http, $modal) {
 
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+
+    //>=, not <=
+    if (scroll >= 20) {
+      $(".header").addClass("scroll-header");
+    }
+    else {
+      $(".header").removeClass("scroll-header");
+    }
+  });
+
   $scope.joinTwerk = function (size, msg) {
     var modalInstance = $modal.open({
       templateUrl: '/views/partials/join-twerk-modal.html',
