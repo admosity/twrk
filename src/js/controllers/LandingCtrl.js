27c2cb731e912a95381a2695fda0c2f962753dff
michaelfortuna <m@ninjastack.com>
2015-05-16 13:12:41 -0400

    added how to twerk modal to homepage

--- src/js/controllers/LandingCtrl.js
+++ src/js/controllers/LandingCtrl.js
@@ -4,5 +4,12 @@ var module = require('./module');
 
 module.controller('LandingCtrl', function($scope, $http, $modal) {
 
+  $scope.joinTwerk = function (size, msg) {
+    var modalInstance = $modal.open({
+      templateUrl: '/views/partials/join-twerk-modal.html',
+      size: size,
+      windowClass: 'join-twerk-modal'
+    });
+  }
 
 });
