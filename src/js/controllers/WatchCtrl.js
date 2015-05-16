75e97bf5a8e5705bfd083b90d413c9418c070fd6
David Kim <davidk@halopowered.com>
2015-05-16 04:37:51 -0400

    watch push

--- src/js/controllers/WatchCtrl.js
+++ src/js/controllers/WatchCtrl.js
@@ -23,65 +23,62 @@ module.controller('WatchCtrl', function($scope, $http, $modal) {
   // var canvas = document.getElementById('myCanvas');
   // var context = canvas.getContext('2d');
 
-  // function drawLine(x, y, z, m){
-  //   var cX = x;// / 2 + y / 2;
-  //   var cY = z;
-  //   var mag = m * 10;
+  function drawLine(x, y, z, m){
+    var cX = x;// / 2 + y / 2;
+    var cY = z;
+    var mag = m * 10;
 
-  //   context.beginPath();
-  //   context.moveTo(400, 300);
-  //   context.lineTo(400 + cX * mag, 300 + cY * mag);
-  //   context.stroke();
-  // }
+    ctx.beginPath();
+    ctx.moveTo(400, 300);
+    ctx.lineTo(400 + cX * mag, 300 + cY * mag);
+    ctx.stroke();
+  }
 
-  // var lp = 0.2;
-  // var lpX = 0;
-  // var lpY = 0;
-  // var lpZ = 0;
+  var lp = 0.2;
+  var lpX = 0;
+  var lpY = 0;
+  var lpZ = 0;
 
+  console.log(window.SERVER_URL);
 
+  var socket = io.connect(window.SERVER_URL);
+  socket.on('connect', function (data) {
+    console.log("CONNECT RESPONSE");
+  });
 
-  // var socket = io.connect(window.SERVER_URL);
-  // socket.on('connect', function (data) {
-  //   console.log("CONNECT RESPONSE");
-  // });
+  socket.on('reply', function (data) {
+    console.log("UPDATE RESPONSE", data);
 
-  // socket.on('reply', function (data) {
-  //   console.log("UPDATE RESPONSE", data);
+    console.log(data);
+    var vector = data.data.split(',');
+    lpX = (vector[0] * (1-lp)) + (lpX * lp);
+    lpY = (vector[1] * (1-lp)) + (lpY * lp);
+    lpZ = (vector[2] * (1-lp)) + (lpZ * lp);
+    ctx.clearRect ( 0 , 0 , canvas.width, canvas.height );
+    ctx.strokeStyle = 'rgba(255, 0, 0, 0.6)';
+    drawLine(vector[0],vector[1],vector[2],vector[6]);
+    ctx.strokeStyle = '#0000ff';
+    drawLine(vector[3],vector[4],vector[5],vector[6]);
+    ctx.strokeStyle = '#00ff00';
+    drawLine(lpX, lpY, lpZ, vector[6]);
 
-  //   console.log(data);
-  //   var vector = data.data.split(',');
-  //   lpX = (vector[0] * (1-lp)) + (lpX * lp);
-  //   lpY = (vector[1] * (1-lp)) + (lpY * lp);
-  //   lpZ = (vector[2] * (1-lp)) + (lpZ * lp);
-  //   context.clearRect ( 0 , 0 , canvas.width, canvas.height );
-  //   context.strokeStyle = 'rgba(255, 0, 0, 0.6)';
-  //   drawLine(vector[0],vector[1],vector[2],vector[6]);
-  //   context.strokeStyle = '#0000ff';
-  //   drawLine(vector[3],vector[4],vector[5],vector[6]);
-  //   context.strokeStyle = '#00ff00';
-  //   drawLine(lpX, lpY, lpZ, vector[6]);
+  });
 
-  // });
+  function drawLine(x, y, z, m){
+    var cX = -(x/Math.abs(x) * (Math.abs(x / 2) + Math.abs(y / 2)));
+    var cY = z;
+    var mag = m * 25;
 
-  // var canvas = document.getElementById('myCanvas');
-  // var context = canvas.getContext('2d');
-
-  // function drawLine(x, y, z, m){
-  //   var cX = -(x/Math.abs(x) * (Math.abs(x / 2) + Math.abs(y / 2)));
-  //   var cY = z;
-  //   var mag = m * 25;
-
-  //   context.beginPath();
-  //   context.lineWidth = 15;
-  //   context.moveTo(400, 300);
-  //   context.lineTo(400 + cX * mag, 300 + cY * mag);
-  //   context.stroke();
-  // }
-  // var lp = 0.2;
-  // var lpX = 0;
-  // var lpY = 0;
-  // var lpZ = 0;
+    ctx.beginPath();
+    ctx.lineWidth = 15;
+    ctx.moveTo(400, 300);
+    ctx.lineTo(400 + cX * mag, 300 + cY * mag);
+    ctx.stroke();
+  }
+  var lp = 0.2;
+  var lpX = 0;
+  var lpY = 0;
+  var lpZ = 0;
 
 
 
@@ -301,16 +298,15 @@ module.controller('WatchCtrl', function($scope, $http, $modal) {
     world.addConstraint(rightElbowJoint);
     
 
-    // Create a body for the cursor
-    var constraintBody = new p2.Body();
-    constraintBody.position[0] = 0.5;
-    constraintBody.position[1] = 0.5;
-    world.addBody(constraintBody);
-    var springConstraint = new p2.RevoluteConstraint(constraintBody, pelvis, {
-      worldPivot: [0.5,0.5],
-      collideConnected:false
-    });
-    world.addConstraint(springConstraint);
+    // // Create a body for the cursor
+    // world.addBody(constraintBody);
+    // var springConstraint = new p2.RotationalSpring(constraintBody, pelvis, {
+    //   worldPivot: [0.5,0.5],
+    //   collideConnected:false
+    // });
+    // world.addConstraint(springConstraint);
+
+    //world.addConstraint(springConstraint);
 
     return {
       body: bodyPartBody, 
@@ -335,6 +331,14 @@ module.controller('WatchCtrl', function($scope, $http, $modal) {
   }
   
   function impulse(player, force){
+
+    var constraintBody = new p2.Body();
+    constraintBody.position[0] = 0.5;
+    constraintBody.position[1] = 0.5;
+    player.body.pelvis.applyForce([.01, .1], player.body.pelvis);
+
+    // var springConstraint = new p2.RotationalSpring(player.body.pelvis, constraintBody, {});
+    // springConstraint.applyForce();
 
   //   // Convert the canvas coordinate to physics coordinates
   //   var position = getPhysicsCoord(event);
@@ -392,6 +396,10 @@ module.controller('WatchCtrl', function($scope, $http, $modal) {
     world.addBody(plane);
 
     console.log("INIT");
+
+    canvas.addEventListener('mousedown', function(event){
+      impulse(players[0]);
+    });
   }
 
   function drawCircle(shape, body){
