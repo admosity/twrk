64e0f5b1d99493cd7dcb23bfaefa5e80b5226996
michaelfortuna <m@ninjastack.com>
2015-05-16 03:09:35 -0400

    added guide modal to watch controller

--- src/js/controllers/LandingCtrl.js
+++ src/js/controllers/LandingCtrl.js
@@ -15,12 +15,4 @@ module.controller('LandingCtrl', function($scope, $http, $modal) {
   //   }
   // });
 
-  $scope.joinTwerk = function (size, msg) {
-    var modalInstance = $modal.open({
-      templateUrl: '/views/partials/join-twerk-modal.html',
-      size: size,
-      windowClass: 'join-twerk-modal'
-    });
-  }
-
 });
--- src/js/controllers/WatchCtrl.js
+++ src/js/controllers/WatchCtrl.js
@@ -7,6 +7,13 @@ module.controller('WatchCtrl', function($scope, $http, $modal) {
   console.log("WATCH DAT TWERK");
 
 
+  $scope.joinTwerk = function (size, msg) {
+    var modalInstance = $modal.open({
+      templateUrl: '/views/partials/join-twerk-modal.html',
+      size: size,
+      windowClass: 'join-twerk-modal'
+    });
+  }
 
 
   var canvas = document.getElementById('myCanvas');
@@ -16,7 +23,7 @@ module.controller('WatchCtrl', function($scope, $http, $modal) {
     var cX = x;// / 2 + y / 2;
     var cY = z;
     var mag = m * 10;
-    
+
     context.beginPath();
     context.moveTo(400, 300);
     context.lineTo(400 + cX * mag, 300 + cY * mag);
@@ -60,7 +67,7 @@ module.controller('WatchCtrl', function($scope, $http, $modal) {
     var cX = -(x/Math.abs(x) * (Math.abs(x / 2) + Math.abs(y / 2)));
     var cY = z;
     var mag = m * 25;
-    
+
     context.beginPath();
     context.lineWidth = 15;
     context.moveTo(400, 300);
@@ -137,12 +144,12 @@ module.controller('WatchCtrl', function($scope, $http, $modal) {
         mass: 1,
         position: [shouldersDistance/2,lowerLegLength / 2],
     });
-    
+
     lowerLeftLeg.addShape(lowerLegShape);
     lowerRightLeg.addShape(lowerLegShape);
     world.addBody(lowerLeftLeg);
     world.addBody(lowerRightLeg);
-    
+
     // Upper legs
     var upperLeftLeg = new p2.Body({
         mass: 1,
