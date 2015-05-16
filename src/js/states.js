a725a77af73aab4330a581423eaace96902e4539
Adam Lee <adam@ninjastack.com>
2015-05-15 22:54:51 -0400

    prepping views

--- src/js/states.js
+++ src/js/states.js
@@ -15,7 +15,69 @@ module.config(['$stateProvider',
           },
 
         }
-      }); // last state
+      })
+
+      .state('watch-twerk', {
+        views: {
+
+          'content': {
+            templateUrl: "/views/watch-twerk.html"
+          },
+
+        }
+      })
+
+      .state('twerk-setup', {
+        views: {
+
+          'content': {
+            templateUrl: "/views/twerk-setup.html"
+          },
+
+        }
+      })
+
+      .state('twerk-calibrate', {
+        views: {
+
+          'content': {
+            templateUrl: "/views/twerk-calibrate.html"
+          },
+
+        }
+      })
+
+      .state('twerk-do', {
+        views: {
+          content: {
+            templateUrl: '/views/twerk-do.html'
+          }
+        }
+      })
+
+
+      .state('twerk-howto', {
+        views: {
+          content: {
+            templateUrl: '/views/twerk-howto.html'
+          }
+        }
+      })
+
+
+      .state('twerk-config', {
+        views: {
+          content: {
+            templateUrl: '/views/twerk-config.html'
+          }
+        }
+      })
+
+
+
+
+
+
 
   }
 ]);
