fd929a2c35b1bab0bb990be67d156674366975b9
David Kim <davidk@halopowered.com>
2015-05-15 22:07:31 -0400

    Fixed server io

--- src/js/app.js
+++ src/js/app.js
@@ -8,8 +8,12 @@ require('./states');
 require('./live');
 
 
-var socket = io.connect('http://localhost:3000');
-socket.on('news', function (data) {
-  console.log(data);
-  socket.emit('my other event', { my: 'data' });
+var socket = io.connect('http://localhost:3000/');
+socket.on('connect', function (data) {
+  console.log("CONNECT RESPONSE");
+  socket.emit('update', { my: 'data' });
+});
+
+socket.on('reply', function (data) {
+  console.log("UPDATE RESPONSE", data);
 });
